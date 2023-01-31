1d339c5cf00ddef5427fed2927306d938c46285e
Matias Tortoriello <matiastortoriello1991@gmail.com>
2023-01-31 15:08:43 -0300

    Arreglo botones

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+let experiencia = document.getElementById('experiencia');
+let formacion = document.getElementById('formacion');
+let otros_conocimientos = document.getElementById('otros-conocimientos');
+
+onload = experiencia.style.display = "none", formacion.style.display="none", otros_conocimientos.style.display = "none";
+
+
 function mostrar(id){
     let seccion = document.getElementById(id)
-
+    
     if (seccion.style.display === "none"){
         seccion.style.display = "block";
     } else {
